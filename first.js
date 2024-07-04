22f884b40ca7cb9fa017b49fb9fdd1934a67ba5b
Anil Kumar Nayak <anilkumarnayak648@gmail.com>
2024-07-04 11:34:58 +0530

    adding new programs

---
+++ first.js
@@ -0,0 +1,16 @@
+var a=5;//it is a number
+function firstprogram()
+{
+    let b=6;
+    console.log(a);
+    console.log(b);
+}
+firstprogram();
+const z=1;//this is a constant variable
+console.log(typeof z);//typeof is return the data type of z
+let student={
+    firstname : "xyz",
+    lastname : "abc",
+    rollno : 1234.
+}
+console.log(student.rollno); //or we can write student["rollno"]
